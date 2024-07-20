19b340eb98b12ea0495c5e28eb23b2ad1ec9c67d
joszuue <100058726+joszuue@users.noreply.github.com>
2024-07-20 12:11:47 -0600

    Add files via upload

---
+++ MiTiendaSystem/js/pedidos.js
@@ -0,0 +1,58 @@
+function removeAccents(str) {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  function filterByDate(dateString, startDate, endDate) {
+    const date = new Date(dateString);
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    return (!startDate || date >= start) && (!endDate || date <= end);
+  }
+
+  function filterCards() {
+    const startDate = document.getElementById('startDate').value;
+    const endDate = document.getElementById('endDate').value;
+    const cards = document.querySelectorAll('.col-sm-4');
+
+    cards.forEach(card => {
+      const order = removeAccents(card.getAttribute('data-order').toLowerCase());
+      const customer = removeAccents(card.getAttribute('data-customer').toLowerCase());
+      const status = removeAccents(card.getAttribute('data-status').toLowerCase());
+      const date = card.getAttribute('data-date');
+
+      const isDateInRange = filterByDate(date, startDate, endDate);
+
+      if (isDateInRange) {
+        card.style.display = '';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  }
+
+  document.getElementById('startDate').addEventListener('change', filterCards);
+  document.getElementById('endDate').addEventListener('change', filterCards);
+
+  document.getElementById('resetButton').addEventListener('click', function() {
+    document.getElementById('startDate').value = '';
+    document.getElementById('endDate').value = '';
+    filterCards(); // Aplicar el filtro después de resetear
+  });
+
+ /* document.getElementById('searchInput').addEventListener('input', function() {
+    const searchValue = this.value.toLowerCase();
+    const cards = document.querySelectorAll('.col-sm-4');
+
+    cards.forEach(card => {
+      const order = card.getAttribute('data-order').toLowerCase();
+      const customer = card.getAttribute('data-customer').toLowerCase();
+      const status = card.getAttribute('data-status').toLowerCase();
+
+      if (order.includes(searchValue) || customer.includes(searchValue) || status.includes(searchValue)) {
+        card.style.display = '';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });*/
