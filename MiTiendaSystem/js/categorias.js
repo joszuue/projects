c8a6d6f6f1eaaa02899c10e17ac362483f1727d4
joszuue <100058726+joszuue@users.noreply.github.com>
2024-07-20 08:28:04 -0600

    Add files via upload

--- MiTiendaSystem/js/categorias.js
+++ MiTiendaSystem/js/categorias.js
@@ -16,25 +16,44 @@ function normalizeText(text) {
     return text.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toUpperCase();
 }
 
-// Buscador categorías
-document.getElementById('searchInput').addEventListener('keyup', function() {
-    var input, filter, table, tr, td, i, j, txtValue;
+// Buscador y filtro
+document.getElementById('searchInput').addEventListener('keyup', filterTable);
+document.getElementById('filterSelect').addEventListener('change', filterTable);
+
+function filterTable() {
+    var input, select, filter, selectFilter, table, tr, td, i, j, txtValue;
     input = document.getElementById('searchInput');
+    select = document.getElementById('filterSelect');
     filter = normalizeText(input.value);
+    selectFilter = select.value;
     table = document.getElementById('cateTable');
     tr = table.getElementsByTagName('tr');
 
     for (i = 0; i < tr.length; i++) {
         tr[i].style.display = "none";
         td = tr[i].getElementsByTagName('td');
+        var matchesSearch = false;
+        var matchesFilter = false;
+
         for (j = 0; j < td.length; j++) {
             if (td[j]) {
                 txtValue = normalizeText(td[j].textContent || td[j].innerText);
-                if (txtValue.indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                    break;
+                if (txtValue.indexOf(filter) > -1 || filter === "") {
+                    matchesSearch = true;
                 }
             }
         }
+
+        var totalProductos = parseInt(td[3].textContent || td[3].innerText, 10);
+        if (selectFilter === "" || 
+            (selectFilter === "mayor" && totalProductos > 0) || 
+            (selectFilter === "igual" && totalProductos === 0)) {
+            matchesFilter = true;
+        }
+
+        if (matchesSearch && matchesFilter) {
+            tr[i].style.display = "";
+        }
     }
-});
+}
+
